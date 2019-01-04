68314de98f779101f1ae1ab729cffb1592418745
Leandro A L Coelho <leandro.coelho@ibge.gov.br>
2019-01-04 14:55:28 -0200

    Commit inicial

---
+++ main.clj
@@ -0,0 +1,61 @@
+(def total-de-vidas 6)
+(def palavra-secreta "MELANCIA")
+
+(defn perdeu [] (print "Você perdeu"))
+(defn ganhou [] (print "Você ganhou!"))
+
+(defn letras-faltantes [palavra acertos] 
+  (remove (fn [letra] (contains? acertos (str letra))) palavra)
+)
+
+(defn acertou-a-palavra-toda? [palavra acertos] 
+  (empty? (letras-faltantes palavra acertos))
+)
+
+(defn le-letra! [] 
+  (do
+    (println "Chute uma letra: ")
+    (read-line)
+  )
+)
+
+(defn acertou? [chute palavra] 
+  (.contains palavra chute)
+)
+
+(defn imprime-forca [vidas palavra acertos]
+  (println "Vidas " vidas)
+  (doseq [letra (seq palavra)] 
+    (if (contains? acertos (str letra))
+      (print letra " ")
+      (print "_" " ")
+    )
+  )
+  (println)
+)
+
+(defn jogo [vidas palavra acertos]
+  (imprime-forca vidas palavra acertos)
+  (cond 
+      (= vidas 0) (perdeu)
+      (acertou-a-palavra-toda? palavra acertos) (ganhou)
+      :else
+        (let [chute (le-letra!)]
+          (if (acertou? chute palavra)
+            (do 
+              (println "Acertou a letra!\n")
+              (recur vidas palavra (conj acertos chute))
+            )
+            (do 
+              (println "Errou a letra! Perdeu vida!\n")
+              (recur (dec vidas) palavra acertos)
+            )
+            
+          )
+        )     
+  )
+)
+
+(defn comeca-o-jogo [] (jogo total-de-vidas palavra-secreta #{}))
+
+;;(jogo 5 "MELANCIA" #{})
